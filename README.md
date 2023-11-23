7b6ddd4cc2fdfc2f78cfa675e5399051d1bc89d8
Draven <lohitkolluri@gmail.com>
2023-11-23 12:51:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# npx_card
-Use npx lohitkolluri to connect with me
+# Lohit Kolluri's NPX Business Card
+
+This is a Node.js command line interface (CLI) business card created by Lohit Kolluri. The CLI provides options to interact with Lohit, such as sending an email, downloading the resume, scheduling a meeting, and more.
+
+## Table of Contents
+
+- [Features](#features)
+- [Usage](#usage)
+- [Reference](#reference)
+- [License](#license)
+
+## Features
+
+- Send an email to Lohit.
+- Download Lohit's resume.
+- Schedule a meeting with Lohit.
+
+## Usage
+
+Run Lohit Kolluri's NPX Business Card directly from the terminal using:
+
+```
+npx lohitkolluri
+```
+## Reference 
+The article written by @jackboberg. I used the same for the reference to deploy the package [Write a Simple npx Business Card](https://studioelsa.se/blog/open-source-oss-npx-business-card). 
---
+++ card.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:hi@anmolsingh.me");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+                value: () => {
+                    // cliSpinners.dots;
+                    const loader = ora({
+                        text: ' Downloading Resume',
+                        spinner: cliSpinners.material,
+                    }).start();
+                    let pipe = request('https://lohitkolluri.tech/resume.pdf').pipe(fs.createWriteStream('./resume.pdf'));
+                    pipe.on("finish", function () {
+                        let downloadPath = path.join(process.cwd(), 'resume.pdf')
+                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
+                        open(downloadPath)
+                        loader.stop();
+                    });
+                }
+            },
+            {
+                name: `Schedule a ${chalk.redBright.bold("Meeting")}?`,
+                value: () => {
+                    open('https://calendly.com/lohitkolluri/30min');
+                    console.log("\n See you at the meeting \n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("             Lohit Kolluri"),
+    handle: chalk.white("@lohitkollluri"),
+    Education: `${chalk.white("Student At")} ${chalk
+        .hex("#2b82b2")
+        .bold("SRM University")}`,
+    github: chalk.gray("https://github.com/") + chalk.green("lohitkolluri"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("kollurilohit"),
+    web: chalk.cyan("https://lohitkolluri.tech/"),
+    npx: chalk.red("npx") + " " + chalk.white("lohitkolluri"), 
+
+    labelWork: chalk.white.bold("       Education:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.Education}`,
+        ``,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I am currently looking for new opportunities,"
+        )}`,
+        `${chalk.italic("my inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
